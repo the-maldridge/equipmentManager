f764467fe9eb2d4d50c897a4a8966d91cc42f58b
Michael Aldridge <aldridge.mac@gmail.com>
2014-10-04 01:51:25 -0500

    now lists checked out items

--- checkin.php
+++ checkin.php
@@ -4,13 +4,21 @@ require("handlers/db_connect.php");
 $SQL="SELECT * FROM checkout WHERE timein IS NULL";
 $itemsout=mysql_query($SQL, $DBCON);
 
-echo "<table>\n";
-echo "<tr><td>Student Name</td></tr>\n";
+echo "<table cellpadding=4>\n";
+echo "<tr><th>Student Name</th><th>Item</th><th>Action</th></tr>\n";
 
 while($row = mysql_fetch_array($itemsout)) {
+  $rid=$row["index"];
   $name=$row["name"];
-  echo "<tr><td>$name</td></tr>";
+  $stuff=$row["item"];
+
+  echo "<tr><td>$name</td><td>$stuff</td>";
+  echo "<td valign=\"middle\"><form action=\"checkin.php\" method=\"get\">\n";
+  echo "\t<input type=hidden value=$rid>\n";
+  echo "\t<input type=Submit value=\"Check in\">\n";
+  echo "</form></td></tr>\n";
 }
+echo "</table>";
 
 
 mysql_close($DBCON);
