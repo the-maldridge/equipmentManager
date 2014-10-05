3422e317f80918d0ce7ca1000056664e26978305
Michael Aldridge <aldridge.mac@gmail.com>
2014-10-05 00:20:32 -0500

    furthur modifications to checkout.php

--- checkout.php
+++ checkout.php
@@ -1,29 +1,61 @@
-<html>
-  <head>
-    <title>Ping Pong Checkout</title>
-  </head>
-  <body>
-    <form action="../handlers/parseform.php" method="get">
-      <?php require("common.php"); ?>
-      <input name="item" type="hidden" value="pingpong">
-      <p>How many Ping Pong balls are being checked out:
-	<select name="quantity">
-	  <option value="1">1</option>
-	  <option value="2">2</option>
-	  <option value="3">3</option>
-	</select>
+<?php
+
+$item=$_GET["item"];
+$formState=$_GET["formState"];
+
+if(empty($item)) {
+  echo '<form action="checkout.php" method="get">
+    <select name="item">
+    <option value="pingpong">Ping Pong</option>
+    <option value="pool">Pool</option>
+    <option value="foosball">FoosBall</option>
+    <option value="kitchen">Kitchen</option>
+    <option value="cart">Cart</option>
+    <option value="room">Room</option>
+    </select>
+    <input type="submit" value="Go!">
+    </form>';
+}
+
+if(empty($formState) && !empty($item)) {
+  //send the base form
+  echo '<form action="checkout.php" method="get">
+    <select name="building">
+    <option value="RHS">Residence Hall South</option>
+    <option value="RHN">Residence Hall North</option>
+    <option value="RHW">Residence Hall West</option>
+    <option value="RHSW">Residence Hall SouthWest</option>
+    <option value="RHNW">Residence Hall NorthWest</option>
+    </select>
+
+    <input type="text" name="name" value="Student Name">
+    <input type="text" name="sid" value="Student ID #">
+    <input name="item" type="hidden" value="$item">';
+  
+  if($item=="pingpong") {
+    echo '
+    <p>How many Ping Pong balls are being checked out:
+    <select name="quantity">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      </select>
       </p>
       <p>How many Ping Pong Paddles are being checked out:
-	<select name="players">
-	  <option value="1">1</option>
-	  <option value="2">2</option>
-	  <option value="3">3</option>
-	  <option value="4">4</option>
-	  <option value="5">5</option>
-	  <option value="6">6</option>
-	</select>
-      </p>
-    <input type="submit" value="Continue!" />
-  </form>
- </body>
-</html>
+    <select name="players">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+      <option value="6">6</option>
+      </select>
+      </p>';
+  }
+
+  //end of form
+  echo '<input type="submit" value="Continue!" />
+      </form>';
+}
+
+?>
