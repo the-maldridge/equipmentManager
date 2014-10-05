83b7243025ddfea93e563bd6d572a73356e5bcb8
Michael Aldridge <aldridge.mac@gmail.com>
2014-10-05 02:45:36 -0500

    fix quoting style

--- checkin.php
+++ checkin.php
@@ -7,8 +7,9 @@ $formState=$_GET["formState"];
 $damage=$_GET["damage"];
 
 if(!empty($confirm) && $confirm=="No") {
-  echo "<meta http-equiv=\"refresh\" content=\"3; url=checkin.php\">";
-  echo "Cancelled checkin; redirecting to checkin main...";
+  //handle the form being cancelled
+  echo '<meta http-equiv=\"refresh" content="3; url=checkin.php">';
+  echo 'Cancelled checkin; redirecting to checkin main...';
 }
 
 if(empty($coutid) && empty($confirm)) {
@@ -17,26 +18,26 @@ if(empty($coutid) && empty($confirm)) {
   $itemsout=mysql_query($SQL, $DBCON);
 
   if(mysql_num_rows($itemsout)==0) {
-    echo "No items are currently checked out";
+    echo 'No items are currently checked out';
   } else {
 
-    echo "<table cellpadding=4 border=2>\n";
-    echo "<tr><th>Student Name</th><th>Item</th><th>Action</th></tr>\n";
+    echo '<table cellpadding=4 border=2>\n';
+    echo '<tr><th>Student Name</th><th>Item</th><th>Action</th></tr>\n';
 
     while($row = mysql_fetch_array($itemsout)) {
       $rid=$row["index"];
       $name=$row["name"];
       $stuff=$row["item"];
       
-      echo "<tr><td>$name</td><td>$stuff</td>";
-      echo "<td valign=\"middle\">\n";
-      echo "\t<form action=\"checkin.php\" method=\"get\">\n";
-      echo "\t<input name=\"coutid\" type=hidden value=$rid>\n";
-      echo "\t<input type=Submit value=\"Check in\">\n";
-      echo "</form></td></tr>\n";
+      echo '<tr><td>$name</td><td>$stuff</td>';
+      echo '<td valign="middle">\n';
+      echo '\t<form action="checkin.php" method="get">\n';
+      echo '\t<input name="coutid" type=hidden value=$rid>\n';
+      echo '\t<input type=Submit value="Check in">\n';
+      echo '</form></td></tr>\n';
     }
     
-    echo "</table>";
+    echo '</table>';
   }
 }
 
@@ -47,22 +48,22 @@ if(!empty($coutid) && empty($confirm)) {
       die("row fetch error");
     } else {
       $data=mysql_fetch_array($row);
-      echo "Are you sure you want to check in " . $data["item"] . " from " . $data["name"];
-      echo "<form action=\"checkin.php\" method=\"get\">";
-      echo "\n\t<input type=\"hidden\" name=\"coutid\" value=$coutid>";
-      echo "\n\t<input type=\"submit\" name=\"confirm\" value=\"No\">";
-      echo "\n\t<input type=\"submit\" name=\"confirm\" value=\"Yes\">";
-      echo "\n</form>";
+      echo 'Are you sure you want to check in ' . $data["item"] . " from " . $data["name"];
+      echo '<form action="checkin.php" method="get">';
+      echo '\n\t<input type="hidden" name="coutid" value=$coutid>';
+      echo '\n\t<input type="submit" name="confirm" value="No">';
+      echo '\n\t<input type="submit" name="confirm" value="Yes">';
+      echo '\n</form>';
     }
 }
 
 if(!empty($coutid) && $confirm=="Yes" && $formState!="Finish") {
-  echo "<form action=\"checkin.php\" method=\"get\">";
-  echo "\n\t<input type=\"hidden\" name=\"coutid\" value=$coutid>";
-  echo "\n\t<input type=\"hidden\" name=\"confirm\" value=$confirm>";
-  echo "\n\tAre any items damaged: <input type=\"checkbox\" name=\"damage\" value=\"Yes\">";
-  echo "\n\t<input type=\"submit\" name=\"formState\" value=\"Finish\">";
-  echo "\n</form>";
+  echo '<form action="checkin.php" method="get">';
+  echo '\n\t<input type="hidden" name="coutid" value=$coutid>';
+  echo '\n\t<input type="hidden" name="confirm" value=$confirm>';
+  echo '\n\tAre any items damaged: <input type="checkbox" name="damage" value="Yes">';
+  echo '\n\t<input type="submit" name="formState" value="Finish">';
+  echo '\n</form>';
 }
 
 if(!empty($formState) && $formState=="Finish") {
@@ -72,8 +73,8 @@ if(!empty($formState) && $formState=="Finish") {
   if(!mysql_query($SQL, $DBCON)) {
     die("Serious checkin error".mysql_error());
   } else {
-    echo "Commit successful; redirecting...";
-    echo "<meta http-equiv=\"refresh\" content=\"3; url=index.html\">";
+    echo 'Commit successful; redirecting...';
+    echo '<meta http-equiv="refresh" content="3; url=index.html">';
   }
 }
  
--- checkout.php
+++ checkout.php
@@ -1,91 +1,110 @@
 <?php
+//this file contains huge echoing sections of html, you've been warned
 
+
+//figure out where we are in the process
 $item=$_GET["item"];
 $formState=$_GET["formState"];
 
+
 if(empty($item)) {
-  echo '<form action="checkout.php" method="get">
-    <select name="item">
-    <option value="pingpong">Ping Pong</option>
-    <option value="pool">Pool</option>
-    <option value="foosball">FoosBall</option>
-    <option value="kitchen">Kitchen</option>
-    <option value="cart">Cart</option>
-    <option value="room">Room</option>
-    </select>
-    <input type="submit" value="Go!">
-    </form>';
+  //we don't currently have state, so send the initial form data
+  echo '<form action="checkout.php" method="get">';
+  echo '<select name="item">';
+  echo '<option value="pingpong">Ping Pong</option>';
+  echo '<option value="pool">Pool</option>';
+  echo '<option value="foosball">FoosBall</option>';
+  echo '<option value="kitchen">Kitchen</option>';
+  echo '<option value="cart">Cart</option>';
+  echo '<option value="room">Room</option>';
+  echo '</select>';
+  echo '<input type="submit" value="Go!">';
+  echo '</form>';
 }
 
 if(empty($formState) && !empty($item)) {
-  //send the base form
-  echo '<form action="checkout.php" method="get">
-    <select name="building">';
+  //we have the item data, but still don't have initial state
+  //need to send the checkout form
+  echo '<form action="checkout.php" method="get">';
+
+  //get the buildings from a data file
+  echo '<select name="building">';
   $buildings=file("data/buildings.txt");
   foreach($buildings as $bldgnum => $buildingName) {
     echo '<option value="'.$buildingName.'">'.$buildingName.'</option>';
   }
-    echo '</select>
-    
-    <input type="text" name="name" value="Student Name">
-    <input type="text" name="sid" value="Student ID #">
-    <input name="item" type="hidden" value="'.$item.'">';
+  echo '</select>';
+
+  //get resident information
+  echo '<input type="text" name="name" value="Student Name">';
+  echo '<input type="text" name="sid" value="Student ID #">';
+
+  //keep track of what we are checking out
+  echo '<input name="item" type="hidden" value="'.$item.'">';
   
+
+  //send form specializations
   if($item=="pingpong") {
-    echo '
-    <p>How many Ping Pong balls are being checked out:
-    <select name="quantity">
-      <option value="1">1</option>
-      <option value="2">2</option>
-      <option value="3">3</option>
-      </select>
-      </p>
-      <p>How many Ping Pong Paddles are being checked out:
-    <select name="players">
-      <option value="1">1</option>
-      <option value="2">2</option>
-      <option value="3">3</option>
-      <option value="4">4</option>
-      <option value="5">5</option>
-      <option value="6">6</option>
-      </select>
-      </p>';
+    echo '<p>How many Ping Pong balls are being checked out:';
+    echo '<select name="quantity">';
+    echo '<option value="1">1</option>';
+    echo '<option value="2">2</option>';
+    echo '<option value="3">3</option>';
+    echo '</select>';
+    echo '</p>';
+    echo '<p>How many Ping Pong Paddles are being checked out:';
+    echo '<select name="players">';
+    echo '<option value="1">1</option>';
+    echo '<option value="2">2</option>';
+    echo '<option value="3">3</option>';
+    echo '<option value="4">4</option>';
+    echo '<option value="5">5</option>';
+    echo '<option value="6">6</option>';
+    echo '</select>';
+    echo '</p>';
+
   } else if($item=="pool") {
-    echo '
-<input type="hidden" name="quantity" value="1">
-<p>How many cues are being checked out:
-<select name="players">
-      <option value="1">1</option>
-      <option value="2">2</option>
-      <option value="3">3</option>
-      <option value="4">4</option>
-</select>
-</p>';
+    echo '<input type="hidden" name="quantity" value="1">';
+    echo '<p>How many cues are being checked out:';
+    echo '<select name="players">';
+    echo '<option value="1">1</option>';
+    echo '<option value="2">2</option>';
+    echo '<option value="3">3</option>';
+    echo '<option value="4">4</option>';
+    echo '</select>';
+    echo '</p>';
+
   } else if($item=="foosball") {
-    echo '
-<input type="hidden" name="quantity" value="1">
-<input type="hidden" name="players" value="1">';
+    echo '<input type="hidden" name="quantity" value="1">';
+    echo '<input type="hidden" name="players" value="1">';
+
   } else if($item=="kitchen") {
-    echo '
-<input type="hidden" name="quantity" value="1">
-<input type="hidden" name="players" value="1">';
+    echo '<input type="hidden" name="quantity" value="1">';
+    echo '<input type="hidden" name="players" value="1">';
+
   } else if($item=="room") {
-    echo '
-<select name="quantity">
-<option value="0">Classroom</option>
-<option value="1">Multipurpose Room (MPR)</option>
-</select>
-<input type="hidden" name="players" value="1">';
+    //couldn't come up with a good solution for rooms, elected to use an ENUM based system
+    //will parse it out in a report if needed
+    echo '<select name="quantity">';
+    echo '<option value="0">Classroom</option>';
+    echo '<option value="1">Multipurpose Room (MPR)</option>';
+    echo '</select>';
+    echo '<input type="hidden" name="players" value="1">';
   }    
 
-  //end of form
-  echo '<input type="submit" value="Continue!" />
-        <input type="hidden" name="formState" value="submit">
-        </form>';
+  //all specializations have been sent, send a closing out for the form
+  echo '<input type="submit" value="Continue!" />';
+  echo '<input type="hidden" name="formState" value="submit">';
+  echo '</form>';
+
 
 } else if($formState=="submit") {
+  //form state has been aquired at finish state
+
+  //time to access the database
   require("db_connect.php");
   
+  //escape anything odd in the input strings
   $item=mysql_real_escape_string($_GET["item"]);
   $bldg=mysql_real_escape_string($_GET['building']);
   $name=mysql_real_escape_string($_GET['name']);
@@ -93,16 +112,18 @@ if(empty($formState) && !empty($item)) {
   $quantity=mysql_real_escape_string($_GET['quantity']);
   $players=mysql_real_escape_string($_GET['players']);
   
+  //compose the SQL for the query
   $SQL = "INSERT INTO checkout (building, name, id, item, quantity, people) VALUES ('$bldg', '$name', '$sid', '$item', '$quantity', '$players')";
-  echo "<br />Executing: " . $SQL;
-  
+
   if(!mysql_query($SQL, $DBCON)) {
+    echo "Attempted to run ".$SQL."\n";
     die("Error: ".mysql_error($DBCON));
   } else {
     echo "<br />Form success, redirecting to main page...";
     echo '<meta http-equiv="refresh" content="3; url=index.html">';
   }
 
+  //remember to close out the database uplink
   mysql_close($DBCON);
 }
 
